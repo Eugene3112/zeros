a9777a35541096378f1e0e80eef24f227d8faedc
Eugene3112 <bertoshevgieniy@gmail.com>
2018-09-30 20:37:49 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let res2 = 0;
+  let n = number;
+  while (numder > 0){
+    number = Math.floor(number / 2);
+    res2 += number;
+
+  }
+  number = n;
+  let res5 = 0;
+  while (number > 0){
+    number = Math.floor(number / 5);
+    res5 += number;
+  }
+  return Math.min(res2, res5);
 }
